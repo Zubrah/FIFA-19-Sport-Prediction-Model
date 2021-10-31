09982a3f85080683be2f35c88105e7ef10db0efa
Zubrah <z.msemo@alusutudent.com>
2021-10-31 16:58:28 +0300

    fixed heroku error

--- request.py
+++ request.py
@@ -3,7 +3,7 @@ import requests
 url = 'http://localhost:5000/predict_api'
 r = requests.post(url, json={'Height': 2, 'Crossing': 9, 'Finishing': 6,
                              'Dribbling': 78, 'Curve': 67, 'Agility': 56,
-                             'Shotpower': 89, 'JUmping': 78, 'Aggression': 90,
+                             'Shotpower': 89, 'Jumping': 78, 'Aggression': 90,
                              'Positioning': 80, 'Vision': 84})
 
 print(r.json())
